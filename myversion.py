19f76c7984f8fb5a35f5d66c952c4fd659d75b11
sugan-saranite <csbs269055@saranathan.ac.in>
2025-08-01 14:52:19 +0530

    Create myversion.py

---
+++ myversion.py
@@ -0,0 +1,20 @@
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    a = 10
+    b = 20
+    print("a + b ", a + b )
+    return "Hello World!"
+
+
+if __name__ == "__main__":
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. You
+    # can configure startup instructions by adding `entrypoint` to app.yaml.
+    app.run(host="127.0.0.1", port=9000, debug=True)
